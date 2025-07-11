551e5171a5eef435d455427cf8e4af9df274f94f
edaild <ch5183sb@gmail.com>
2025-07-11 12:10:44 +0900

    Feat : Client 주사위 로직 2차

--- GameNetworkServerUnity/Assets/Sctipts/Client/Dcie/Dice.cs
+++ GameNetworkServerUnity/Assets/Sctipts/Client/Dcie/Dice.cs
@@ -7,7 +7,23 @@ public class Dice : MonoBehaviour
     [SerializeField] private float rotatespeed = 5f;
     [SerializeField] private AudioSource DiceSound;
 
+    public int diceIndex = -1;
+    public DiceManager diceManager;
+
     private bool isbord;
+
+    private void Awake()
+    {
+        if (!diceManager)
+        {
+            diceManager = FindAnyObjectByType<DiceManager>();
+            if( diceManager == null)
+            {
+                Debug.LogError("diceManger을 찾을 수 없습니다.");
+            }
+        }
+
+    }
 
     private void Update()
     {
@@ -15,7 +31,6 @@ public class Dice : MonoBehaviour
         {
             transform.Rotate(rotatespeed * Time.deltaTime, 0, rotatespeed * Time.deltaTime);
         }
-
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -29,9 +44,74 @@ public class Dice : MonoBehaviour
         }
     }
 
-    void countDice()
+    int showDiceCout()
     {
-        Debug.Log("주사위 위쪽 숫자 출력");
+        Vector3 worldUp = Vector3.up;
+
+        float maxDot = -1f;
+        int result = 0;
+
+    if (Vector3.Dot(transform.up, worldUp) > maxDot)
+    {
+        maxDot = Vector3.Dot(transform.up, worldUp);
+        result = 2; // up이 2
+    }
+
+    if (Vector3.Dot(-transform.up, worldUp) > maxDot)
+    {
+        maxDot = Vector3.Dot(-transform.up, worldUp);
+        result = 5; // down이 5
+    }
+
+    if (Vector3.Dot(transform.forward, worldUp) > maxDot)
+    {
+        maxDot = Vector3.Dot(transform.forward, worldUp);
+        result = 1; // forward가 1
+    }
+
+    if (Vector3.Dot(-transform.forward, worldUp) > maxDot)
+    {
+        maxDot = Vector3.Dot(-transform.forward, worldUp);
+        result = 6; // back이 6
+    }
+
+    if (Vector3.Dot(-transform.right, worldUp) > maxDot)
+    {
+        maxDot = Vector3.Dot(-transform.right, worldUp);
+        result = 3; // left가 3
+    }
+
+    if (Vector3.Dot(transform.right, worldUp) > maxDot)
+    {
+        maxDot = Vector3.Dot(transform.right, worldUp);
+        result = 4; // right가 4
+    }
+
+    return result;
+    }
+    public void countDice()
+    {
+        int value = showDiceCout();
+        Debug.Log("주사위 수는: " + value);
+
+        if(diceIndex >= 0 && diceIndex < diceManager.dicecount.Length)
+        {
+            diceManager.dicecount[diceIndex] = value;
+        }
+        else
+        {
+            Debug.LogWarning("주사위 인덱스가 유효하지 않다. : " + diceIndex);
+        }
+
+        if (diceManager.dicecount[0] != 0 && diceManager.dicecount[1] != 0)
+        {
+            diceManager.DiceNumber01 = diceManager.dicecount[0];
+            diceManager.DiceNumber02 = diceManager.dicecount[1];
+
+            Debug.Log($"첫번째 주사위의 수: " + diceManager.dicecount[0]);
+            Debug.Log($"두번째 주사위의 수: " + diceManager.dicecount[1]);
+            Debug.Log($"두 주사위의 합의 수: {diceManager.DiceNumber01 + diceManager.DiceNumber02}");
+        }
     }
 
     void delectDice()
--- GameNetworkServerUnity/Assets/Sctipts/Client/Dcie/DiceManager.cs
+++ GameNetworkServerUnity/Assets/Sctipts/Client/Dcie/DiceManager.cs
@@ -5,9 +5,11 @@ public class DiceManager : MonoBehaviour
 {
     [SerializeField] private Transform DicePrifab;
     [SerializeField] private Transform DicePoint;
+     public int DiceNumber01;
+     public int DiceNumber02;
 
     private bool isTurn;
-
+    public int[] dicecount = new int[2];
 
     private void Start()
     {
@@ -20,7 +22,10 @@ public class DiceManager : MonoBehaviour
             for(int i = 0; i < 2; i++)
             {
                 Instantiate(DicePrifab, DicePoint.position, Quaternion.identity);
+                Dice dice = FindAnyObjectByType<Dice>();
+                dice.diceIndex = i; 
             }
         }
+       
     }
 }
